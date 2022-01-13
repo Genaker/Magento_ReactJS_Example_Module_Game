29cb12f0e17b97efb84a0c02fcffa0dac715361c
Oracle Public Cloud User <opc@instance-20210831-1208.subnet05272019.vcn05272019.oraclevcn.com>
2022-01-13 04:40:28 +0000

    EmojiGame

---
+++ Block/EmojiGame.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Genaker\EmojiGame\Block;
+
+class EmojiGame extends \Magento\Framework\View\Element\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getGame()
+    {
+        //Your block code
+        return __('Hello Developer! This how to get the storename: %1 and this is the way to build a url: %2', $this->_storeManager->getStore()->getName(), $this->getUrl('contacts'));
+    }
+}
+
